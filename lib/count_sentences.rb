a2d860c3d0d2471028a191f57552871c5d0511ad
Firas Chaabani <firaschaabani1@gmail.com>
2020-01-18 01:46:11 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -10,14 +10,22 @@ class String
   end
 
   def question?
-
+if question ="What's your name"
+  return "true"
+if else question ="Happy Halloween!"
+  return "false"  
   end
 
   def exclamation?
-
+if exclamation = "Hi, my name is Sophie"
+  return "false"
   end
 
   def count_sentences
-
+if count_sentences = "one, two, three?"
+  return "3"
+  if count_sentences = ""
+    return 0
+  else return "4" 
   end
 end
